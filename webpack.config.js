9a1b94522bcb59338d50329847cb8ec842816a0e
lens0021 <lorentz0021@gmail.com>
2021-11-22 16:23:23 +0900

    Use webpack, typescrpt and scss

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  optimization: {
+    minimize: false,
+  },
+};
