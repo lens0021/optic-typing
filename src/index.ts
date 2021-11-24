b4fd130dfe3ba62381a42635b1dad1f19f9a5a27
lens0021 <lorentz0021@gmail.com>
2021-11-24 18:39:22 +0900

    Add notification for every 10 min

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { OpticTyping } from './OpticTyping';
 import './styles.scss';
 const Cookies: any = require('js-cookie');
+const tenMinutes = 1000 * 60 * 10;
 
 (() => {
   const $body = document.body;
@@ -38,5 +39,19 @@ const Cookies: any = require('js-cookie');
     }
 
     opticTyping.main();
+    let message = "You've been playing this for 10 minutes.";
+    const startCheckpoint = () => {
+      const $checkpoint = document.createElement('div');
+      $checkpoint.classList.add('checkpoint');
+      $checkpoint.innerHTML = message;
+      message = "You've been playing this for 10 minutes since the last notification.";
+      $canvas.append($checkpoint);
+      setTimeout(() => {
+        $canvas.removeChild($checkpoint);
+      }, 5000);
+      setTimeout(startCheckpoint, tenMinutes);
+    };
+
+    setTimeout(startCheckpoint, tenMinutes);
   }
 })();
